dd7a80716d792560fe219934008ca7c0dc4b7b7c
Guzel N <nabiullinaguzel93@gmail.com>
2026-04-28 05:07:05 +0300

    end

--- js/main.js
+++ js/main.js
@@ -1,4 +1,84 @@
+function toggleCollapsible(btn){
+  btn.closest('.blockCollapsible').classList.toggle('active');
+}
+
+
 const swiper1 = new Swiper('.swiper-1', {
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    576: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 20
+    },
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 30,
+    },
+  }
+});
+
+const swiper2 = new Swiper('.swiper-2', {
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+  }
+});
+
+const swiper3 = new Swiper('.swiper-3', {
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    576: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    993: {
+      slidesPerView: 3,
+      spaceBetween: 20
+    },
+  }
+});
+
+const swiper4 = new Swiper('.swiper-4', {
   loop: true,
   slidesPerView: 1,
   spaceBetween: 20,
